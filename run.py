fdc31ce5ec9e6f1115669da33e3bbda1c9c733c5
Raluca Nechita <nechita.ralucaelena@gmail.com>
2024-08-16 09:57:46 +0000

    Function to print game board

--- run.py
+++ run.py
@@ -5,3 +5,9 @@ import random
 # Define the size of the board and create a 5x5 grid filled with 'O'
 board_size = 5
 board = [['O'] * board_size for _ in range(board_size)]
+
+# Function to print game board
+
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
